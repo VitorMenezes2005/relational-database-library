72cb68a0d954a222f5fbc2be55104c1404cdec5c
Vitor Menezes <vitorcarlos2005@outlook.com>
2026-04-03 20:23:02 -0300

    metodos da entidade book

--- src/main/java/com/vitor/relational_database_library/controllers/BookController.java
+++ src/main/java/com/vitor/relational_database_library/controllers/BookController.java
@@ -5,11 +5,13 @@ import com.vitor.relational_database_library.entites.dto.CreateBookDto;
 import com.vitor.relational_database_library.services.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.URI;
+import java.util.List;
 
 @Controller
 @RequestMapping("/v1/book")
@@ -21,4 +23,17 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @PostMapping
+    public ResponseEntity<Book> registerBook(@RequestBody CreateBookDto dto){
+        var bookId = bookService.register(dto);
+
+        return ResponseEntity.created(URI.create("/v1/book/" + bookId.toString())).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> listBooks(){
+        List<Book> list = bookService.listAll();
+
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/vitor/relational_database_library/services/BookService.java
+++ src/main/java/com/vitor/relational_database_library/services/BookService.java
@@ -5,6 +5,8 @@ import com.vitor.relational_database_library.entites.dto.CreateBookDto;
 import com.vitor.relational_database_library.repositories.BookRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -14,5 +16,22 @@ public class BookService {
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public UUID register(CreateBookDto dto) {
+
+        var entity = new Book(
+                null,
+                dto.name(),
+                dto.genre(),
+                dto.year());
+
+        var id = bookRepository.save(entity);
+
+        return id.getBookId();
+    }
+
+    public List<Book> listAll() {
+        return bookRepository.findAll();
     }
 }

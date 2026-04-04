b84123833af499cf6c90ca2175ad09f86513a959
Vitor Menezes <vitorcarlos2005@outlook.com>
2026-04-04 08:38:40 -0300

    author -> métodos

--- src/main/java/com/vitor/relational_database_library/controllers/AuthorController.java
+++ src/main/java/com/vitor/relational_database_library/controllers/AuthorController.java
@@ -1,10 +1,17 @@
 package com.vitor.relational_database_library.controllers;
 
 import com.vitor.relational_database_library.entites.Author;
+import com.vitor.relational_database_library.entites.dto.CreateAuthorDto;
 import com.vitor.relational_database_library.services.AuthorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+import java.util.List;
 
 @Controller
 @RequestMapping("/v1/author")
@@ -14,5 +21,19 @@ public class AuthorController {
 
     public AuthorController(AuthorService authorService) {
         this.authorService = authorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> registerAuthor(@RequestBody CreateAuthorDto dto){
+        var authorId = authorService.register(dto);
+
+        return ResponseEntity.created(URI.create("/v1/author/" + authorId.toString())).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Author>> listAuthors(){
+        List<Author> list = authorService.listAll();
+
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/vitor/relational_database_library/services/AuthorService.java
+++ src/main/java/com/vitor/relational_database_library/services/AuthorService.java
@@ -1,7 +1,35 @@
 package com.vitor.relational_database_library.services;
 
+import com.vitor.relational_database_library.entites.Author;
+import com.vitor.relational_database_library.entites.dto.CreateAuthorDto;
+import com.vitor.relational_database_library.repositories.AuthorRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AuthorService {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public UUID register(CreateAuthorDto dto){
+
+        var entity = new Author(
+                null,
+                dto.name(),
+                dto.nationality());
+
+        var authorId = authorRepository.save(entity);
+
+        return authorId.getAuthorId();
+    }
+
+    public List<Author> listAll(){
+        return authorRepository.findAll();
+    }
 }

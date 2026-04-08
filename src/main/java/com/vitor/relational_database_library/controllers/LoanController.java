03bd9a10993e48d26b9c28d171ceb7e970d42cd8
Vitor Menezes <vitorcarlos2005@outlook.com>
2026-04-08 05:59:08 -0300

    loan -> metodo de registro

--- src/main/java/com/vitor/relational_database_library/controllers/LoanController.java
+++ src/main/java/com/vitor/relational_database_library/controllers/LoanController.java
@@ -1,8 +1,15 @@
 package com.vitor.relational_database_library.controllers;
 
+import com.vitor.relational_database_library.entites.Loan;
+import com.vitor.relational_database_library.entites.dto.CreateLoanDto;
 import com.vitor.relational_database_library.services.LoanService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
 
 @Controller
 @RequestMapping("/v1/loan")
@@ -12,5 +19,12 @@ public class LoanController {
 
     public LoanController(LoanService loanService) {
         this.loanService = loanService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Loan> registerLoan(@RequestBody CreateLoanDto dto){
+        var loanId = loanService.register(dto);
+
+        return ResponseEntity.created(URI.create("/v1/book/" + loanId.toString())).build();
     }
 }
--- src/main/java/com/vitor/relational_database_library/services/LoanService.java
+++ src/main/java/com/vitor/relational_database_library/services/LoanService.java
@@ -1,7 +1,12 @@
 package com.vitor.relational_database_library.services;
 
+import com.vitor.relational_database_library.entites.Loan;
+import com.vitor.relational_database_library.entites.dto.CreateLoanDto;
 import com.vitor.relational_database_library.repositories.LoanRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class LoanService {
@@ -10,5 +15,17 @@ public class LoanService {
 
     public LoanService(LoanRepository loanRepository) {
         this.loanRepository = loanRepository;
+    }
+
+    public UUID register(CreateLoanDto dto){
+
+        var entity = new Loan(
+                null,
+                Instant.now(),
+                dto.description());
+
+        var id = loanRepository.save(entity);
+
+        return id.getLoanId();
     }
 }

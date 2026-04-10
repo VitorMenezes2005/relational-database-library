0c538597ce89ab18728c757446aa175fa2f292b7
Vitor Menezes <vitorcarlos2005@outlook.com>
2026-04-09 22:01:28 -0300

    client -> métodos

--- src/main/java/com/vitor/relational_database_library/controllers/ClientController.java
+++ src/main/java/com/vitor/relational_database_library/controllers/ClientController.java
@@ -1,8 +1,17 @@
 package com.vitor.relational_database_library.controllers;
 
+import com.vitor.relational_database_library.entites.Client;
+import com.vitor.relational_database_library.entites.dto.CreateClientDto;
 import com.vitor.relational_database_library.services.ClientService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+import java.util.List;
 
 @Controller
 @RequestMapping("/v1/client")
@@ -12,5 +21,19 @@ public class ClientController {
 
     public ClientController(ClientService clientService) {
         this.clientService = clientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> registerClient(@RequestBody CreateClientDto dto){
+        var clientId = clientService.register(dto);
+
+        return ResponseEntity.created(URI.create("/v1/book/" + clientId.toString())).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Client>> listAll(){
+        List<Client> list = clientService.listClients();
+
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/vitor/relational_database_library/services/ClientService.java
+++ src/main/java/com/vitor/relational_database_library/services/ClientService.java
@@ -1,7 +1,12 @@
 package com.vitor.relational_database_library.services;
 
+import com.vitor.relational_database_library.entites.Client;
+import com.vitor.relational_database_library.entites.dto.CreateClientDto;
 import com.vitor.relational_database_library.repositories.ClientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ClientService {
@@ -10,5 +15,23 @@ public class ClientService {
 
     public ClientService(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
+    }
+
+    public UUID register(CreateClientDto dto){
+
+        var entitu = new Client(
+                null,
+                dto.name(),
+                dto.email(),
+                dto.password(),
+                null);
+
+        var id = clientRepository.save(entitu);
+
+        return id.getClienteId();
+    }
+
+    public List<Client> listClients(){
+        return clientRepository.findAll();
     }
 }
